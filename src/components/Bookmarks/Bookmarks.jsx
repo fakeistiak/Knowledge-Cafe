60241e37e4ed22372fc40c3440ffa9eb881ee2f9
fakeistiak <istiakahmedb100.com>
2023-09-13 20:04:25 +0600

    main added

--- src/components/Bookmarks/Bookmarks.jsx
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -2,7 +2,7 @@
 const Bookmarks = () => {
     return (
         <div className="md:w-1/3">
-            <h1>Bookmarks</h1>
+            <h1></h1>
         </div>
     );
 };
--- src/components/blog/blog.jsx
+++ src/components/blog/blog.jsx
@@ -1,15 +1,38 @@
-
-const blog = ({ blog }) => {
-    console.log(blog)
+import PropTypes from 'prop-types'
+import { FaBookmark} from 'react-icons/fa'
+const Blog = ({ blog }) => {
+    const { title,cover,author,author_img,posted_date,reading_time,hashtags} = blog;
     return (
-        <div>
-            
+        <div className='mb-20'>
+            <img className='h-96 w-full mb-8' src={cover} alt={`cover picture of title ${title}`} />
+            <div className='flex justify-between mb-4'>
+                <div className='flex '>
+                    <img className='w-14 rounded-3xl' src={author_img} alt="" />
+                
+                <div className='ml-6'>
+                    <h3 className='text-2xl'>{author}</h3>
+                    <p>{posted_date}</p>
+                    </div>
+                    </div>
+                <div>
+                    <span>{reading_time}min read</span>
+                    <button className='ml-2 font-'><FaBookmark/></button>
+                </div>
+                
+               
+            </div>
+            <h2 className="text-4xl">{title}</h2>
+            <p>
+                {
+                    hashtags.map((hash,idx) => <span key={idx}> <a href="">#{hash}</a></span>)
+                }
+            </p>
         </div>
     );
 };
 
-blog.propTypes = {
+Blog.propTypes = {
     blog:PropTypes.object.isRequired
 }
 
-export default blog;
+export default Blog;
